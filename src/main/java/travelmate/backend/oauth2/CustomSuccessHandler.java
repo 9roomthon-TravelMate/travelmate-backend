dafd44eb8af1af5e8874c3e7a17123839ac9b7b8
ulthwjdwkd <kimsojeoung86@gmail.com>
2024-07-26 16:37:05 +0900

    fix: redirect port 변경

--- src/main/java/travelmate/backend/oauth2/CustomSuccessHandler.java
+++ src/main/java/travelmate/backend/oauth2/CustomSuccessHandler.java
@@ -39,7 +39,7 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 
         response.addCookie(createCookie("Authorization", token));
 //        response.sendRedirect("http://localhost:3000/");
-        response.sendRedirect("http://ec2-43-202-20-181.ap-northeast-2.compute.amazonaws.com:3000");
+        response.sendRedirect("http://ec2-43-202-20-181.ap-northeast-2.compute.amazonaws.com");
     }
 
     private Cookie createCookie(String key, String value) {
