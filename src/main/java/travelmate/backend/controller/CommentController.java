0d79ce158ee2296a20ccacf604411dce3975d758
honge22e <haemin1107@gmail.com>
2024-08-13 13:40:20 +0900

    fix : comment json bug

--- src/main/java/travelmate/backend/controller/CommentController.java
+++ src/main/java/travelmate/backend/controller/CommentController.java
@@ -1,5 +1,6 @@
 package travelmate.backend.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -7,11 +8,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import travelmate.backend.dto.CommentDto;
 import travelmate.backend.dto.CustomOAuth2User;
 import travelmate.backend.entity.Comment;
 import travelmate.backend.repository.CommentRepository;
 import travelmate.backend.service.CommentService;
+
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/comment")
@@ -23,7 +27,12 @@ public class CommentController {
     private CommentService commentService;
 
     @PostMapping("/create")
-    public ResponseEntity<Comment> create(@AuthenticationPrincipal CustomOAuth2User user, @RequestBody CommentDto commentDto) {
+    public ResponseEntity<Comment> create(@AuthenticationPrincipal CustomOAuth2User user,
+                                          @RequestParam("commentDto") String commentDtoJson) throws IOException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        CommentDto commentDto = objectMapper.readValue(commentDtoJson, CommentDto.class);
+
         Comment createdcomment = commentService.createcomment(user, commentDto);
         return ResponseEntity.ok(createdcomment);
     }
